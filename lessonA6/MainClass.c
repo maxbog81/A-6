70354bbbe781a8657eee812c21a13678de3a4404
maxbog81 <maxbog@inbox.ru>
2019-04-08 13:11:28 +0300

    Урок 6

--- lessonA6/MainClass.c
+++ lessonA6/MainClass.c
@@ -2,12 +2,25 @@
 #include <stdio.h>
 #include <locale.h>
 
+int HashString(char str[]) 
+{
+	int sum = 0;
+
+	for (int i = 0; i < strlen(str); ++i)
+		sum += str[i];
+
+	return sum;
+}
 
 int main() {
-	setlocale(LC_ALL, "Rus");
+	//setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "en_US.UTF-8");
 
 //1. Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе получается сумма кодов символов.
-	printf("cdcd");
+
+	unsigned char txt[] = "Hello world!";
+	printf("Cумма кодов символов:%d", HashString(txt));
+	printf("\n");
 	system("pause");
 	return 0;
 }
