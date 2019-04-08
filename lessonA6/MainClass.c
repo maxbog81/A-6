0a5d4828847cefb923a6f5376272f5b779362d02
maxbog81 <maxbog@inbox.ru>
2019-04-08 12:14:04 +0300

    Урок 6

---
+++ lessonA6/MainClass.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+
+//1. Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе получается сумма кодов символов.
+	printf("cdcd");
+	system("pause");
+	return 0;
+}
